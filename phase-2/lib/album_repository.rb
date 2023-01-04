bc648841590d7619134ea86fa994b050d8b79c77
alexHoggett <alexmh99@gmail.com>
2023-01-04 15:47:31 +0000

    Finished writing tests for repo classes

--- phase-2/lib/album_repository.rb
+++ phase-2/lib/album_repository.rb
@@ -1,49 +1,64 @@
+require "album"
+
 class AlbumRepository
   # Selecting all records
   # No argumuments
   def all
     # Executes the sql query:
-    # SELECT * from albums;
+    sql = "SELECT * FROM albums"
+    result_set = DatabaseConnection.exec_params(sql, [])
 
-    # Returns an array of album objects
+    albums = []
+
+    result_set.each do |record|
+      album = Album.new
+      album.id = record['id'].to_i
+      album.title = record['title']
+      album.release_year = record['release_year'].to_i
+      album.artist_id = record['artist_id'].to_i
+      albums << album
+    end
+
+    return albums
   end
 
   # Gets a single record by its ID
   # One arg: id (number)
   def find(id)
-    # Executes the sql query:
-    # SELECT * from albums where id = $id;
+    sql = "SELECT * FROM albums WHERE id = #{id};"
+    result_set = DatabaseConnection.exec_params(sql, [])
 
-    # Returns an album object
+    album = Album.new
+    album.id = result_set[0]['id'].to_i
+    album.title = result_set[0]['title']
+    album.release_year = result_set[0]['release_year'].to_i
+    album.artist_id = result_set[0]['artist_id'].to_i
+
+    return album
   end
 
   # adds an album
   # 3 args: title (string) release_year (number) artist_id (number)
   def create(title, release_year, artist_id)
-    # Executes the sql query:
-    # INSERT INTO albums (title, release_year, artist_id)
-    # VALUES ($title, $release_year, $artist_id)
-
-    # Returns nothing
+    sql = "INSERT INTO albums (title, release_year, artist_id)
+          VALUES ('#{title}', #{release_year}, #{artist_id});"
+    DatabaseConnection.exec_params(sql, [])
   end
 
   # deletes an album
   # 1 arg: id (number)
   def delete(id)
-    # Executes the sql query:
-    # DELETE FROM albums WHERE id = $id;
-
-    # Returns nothing
+    sql = "DELETE FROM albums WHERE id = #{id};"
+    DatabaseConnection.exec_params(sql, [])
   end
 
   # updates an album
   # 3 args: id (number) column (string), value (number | string)
   def update(id, column, value)
-    # Executes the sql query:
-    # UPDATE albums
-    # SET $column = $value
-    # WHERE id = $id;
+    sql = "UPDATE albums
+           SET #{column} = '#{value}'
+           WHERE id = #{id};"
 
-    # Returns nothing
+    DatabaseConnection.exec_params(sql, [])
   end
 end
--- phase-2/spec/album_repository_spec.rb
+++ phase-2/spec/album_repository_spec.rb
@@ -11,8 +11,56 @@ describe AlbumRepository do
     reset_albums_table
   end
 
-  it "" do
+  it "returns all albums" do
+    repo = AlbumRepository.new
+    albums = repo.all
+    
+    expect(albums[0].id).to eq 1
+    expect(albums[0].title).to eq 'Doolittle'
+    expect(albums[0].release_year).to eq 1989
+    expect(albums[0].artist_id).to eq 1
 
+    expect(albums[1].id).to eq 2
+    expect(albums[1].title).to eq 'Super Trouper'
+    expect(albums[1].release_year).to eq 1980
+    expect(albums[1].artist_id).to eq 2
   end
 
+  it "returns a single record by its ID" do
+    repo = AlbumRepository.new
+
+    album = repo.find(1)
+
+
+    expect(album.id).to eq 1
+    expect(album.title).to eq 'Doolittle'
+    expect(album.release_year).to eq 1989
+    expect(album.artist_id).to eq 1
+  end
+
+  it "Adds an album to the abums table" do
+    repo = AlbumRepository.new
+    repo.create('Hot Pink', 2019, 3)
+    albums = repo.all
+    expect(albums.length).to eq 3
+    expect(albums[-1].title).to eq 'Hot Pink'
+    expect(albums[-1].release_year).to eq 2019
+    expect(albums[-1].artist_id).to eq 3
+  end
+
+  it "Deletes an album from the albums table" do
+    repo = AlbumRepository.new
+    albums = repo.all
+    expect(albums.length).to eq 2
+    repo.delete(1)
+    albums = repo.all
+    expect(albums.length).to eq 1
+  end
+
+  it "Updates an album" do
+    repo = AlbumRepository.new
+    repo.update(1, 'title', 'Doolittler')
+    album = repo.find(1)
+    expect(album.title).to eq 'Doolittler'
+  end
 end
