bce8916d76bf438f24661c7055442ad3382a3100
alexHoggett <alexmh99@gmail.com>
2023-01-05 13:56:58 +0000

    finished challenge of part 2

--- phase-2/book_store/lib/book_repository.rb
+++ phase-2/book_store/lib/book_repository.rb
@@ -1,5 +1,49 @@
 require 'Book'
 
 class BookRepository
+  def all
+    sql = "SELECT * FROM books"
+    result_set = DatabaseConnection.exec_params(sql, [])
 
+    books = []
+
+    result_set.each do |record|
+      book = Book.new
+      book.id = record['id'].to_i
+      book.title = record['title']
+      book.author_name = record['author_name']
+      books << book
+    end
+
+    return books
+  end
+
+  def find(id)
+    sql = "SELECT * FROM books WHERE id = #{id}"
+    result_set = DatabaseConnection.exec_params(sql, [])
+
+    book = Book.new
+    book.id = result_set[0]['id'].to_i
+    book.title = result_set[0]['title']
+    book.author_name = result_set[0]['author_name']
+    return book
+  end
+
+  def create(book)
+    sql = "INSERT INTO books (title, author_name) VALUES ('#{book.title}', '#{book.author_name}')"
+    result_set = DatabaseConnection.exec_params(sql, [])
+  end
+
+  def update (id, column, value)
+    sql = "UPDATE books
+           SET #{column} = '#{value}'
+           WHERE id = #{id};"
+
+    DatabaseConnection.exec_params(sql, [])
+  end
+
+  def delete(id)
+    sql = "DELETE FROM books WHERE id = #{id};"
+    DatabaseConnection.exec_params(sql, [])
+  end
 end
--- phase-2/book_store/spec/book_repository_spec.rb
+++ phase-2/book_store/spec/book_repository_spec.rb
@@ -2,7 +2,7 @@ require 'book_repository'
 
 def reset_books_table
   seed_sql = File.read('spec/seeds_books.sql')
-  connection = PG.connect({ host: '127.0.0.1', dbname: 'book_store' })
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'book_store_test' })
   connection.exec(seed_sql)
 end
 
@@ -12,6 +12,54 @@ describe BookRepository do
   end
 
   it "returns all records" do
+    repo = BookRepository.new
+    books = repo.all
 
+    expect(books[0].title).to eq 'Lanark'
+    expect(books[0].author_name).to eq 'Alasdair Gray'
+
+    expect(books[1].id).to eq 2
+    expect(books[1].title).to eq 'Dubliners'
+    expect(books[1].author_name).to eq 'James Joyce'
+  end
+
+  it "gets a single book by its ID" do
+    repo = BookRepository.new
+    book = repo.find(1)
+
+    expect(book.id).to eq 1
+    expect(book.title).to eq 'Lanark'
+    expect(book.author_name).to eq 'Alasdair Gray'
+  end
+
+  it "adds a book to the table" do
+    repo = BookRepository.new
+
+    book = Book.new
+    book.title = 'Burning Patience'
+    book.author_name = 'Antonio Skarmeta'
+    
+    repo.create(book)
+    
+    books = repo.all
+    expect(books[-1].title).to eq 'Burning Patience'
+    expect(books[-1].author_name).to eq 'Antonio Skarmeta'
+  end
+
+  repo = BookRepository.new
+
+  it "Updates a book in the table" do
+    repo.update(1, 'title', 'Scolding Patience')
+    book = repo.find(1)
+    expect(book.title).to eq 'Scolding Patience'
+  end
+
+  it "deletes a book from the table" do
+    repo = BookRepository.new
+    expect(repo.all.length).to eq 2
+    repo.delete(1)
+    books = repo.all
+    expect(books.length).to eq 1
+    expect(books[0].id).to eq 2
   end
 end
